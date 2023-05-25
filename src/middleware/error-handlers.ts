3aa37e063544cc3e8fbe952dd93527294fad21aa
CFriskDreemurr <81151273+CFriskDreemurr@users.noreply.github.com>
2023-05-25 11:04:25 +0200

    Update error-handlers.ts

--- src/middleware/error-handlers.ts
+++ src/middleware/error-handlers.ts
@@ -7,13 +7,13 @@ interface Error2 extends Error {
 }
 
 
-export const notFoundErrorHandler = (req:express.Request, res:express.Response, next:NextFunction) => {
+export const notFoundErrorHandler = (req:express.Request, res:express.Response, next:express.NextFunction) => {
     const error = new Error(`${req.method} ${req.originalUrl} not found`) as Error2;
     error['status'] = 404;
     next(error);
 }
 
-export const globalErrorHandler = (error:Error2, req:express.Request, res:express.Response, next:NextFunction) => {
+export const globalErrorHandler = (error:Error2, req:express.Request, res:express.Response, next:express.NextFunction) => {
     console.log(error.message);
     res.status(error['status'] || 500);
     res.json({ error: error.message });
